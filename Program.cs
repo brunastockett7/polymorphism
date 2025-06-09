27e5dfeb5edb7c9710e538e9234f5e2fa1faaa75
brunastockett7 <bruna__stockett@outlook.com>
2025-06-09 06:40:15 -0800

    Initial commit: Add polymorphism project files

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 4));
+        shapes.Add(new Rectangle("Blue", 4, 5));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Shape area: {shape.GetArea():F2}\n");
+        }
+    }
+}
+
